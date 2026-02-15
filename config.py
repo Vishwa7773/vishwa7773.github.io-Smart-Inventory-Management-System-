4c40b0d9d496223b8f68c30aef52a4637fa1263f
Vishwajeet B <vishwajeetbaing2005@gmail.com>
2026-02-15 19:56:56 +0530

    Add database configuration and secret key

---
+++ config.py
@@ -0,0 +1,6 @@
+SQLALCHEMY_DATABASE_URI = (
+    "postgresql+psycopg2://inventory_user:password@localhost:5432/inventory_db"
+)
+
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SECRET_KEY = "inventory_secret"
